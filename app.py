5c567fd4cd18794fff00f328d6cd95951bafecef
egormeister02 <smirnov.ei@phystech.edu>
2024-09-12 23:56:05 +0300

    fix problem with ip adress

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def send_telegram_message(chat_id, text):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     
     # URL для открытия мини-приложения
-    mini_app_url = f"https://46.8.230.15.nip.io/form?chat_id={chat_id}"
+    mini_app_url = f"https://95.182.98.91.nip.io/form?chat_id={chat_id}"
     
     # Кнопка для мини-приложения
     inline_keyboard = {
